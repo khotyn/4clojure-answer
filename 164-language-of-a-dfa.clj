e3dc39b16a938f1baba110b55f63e075930142cb
khotyn <hting1@gmail.com>
2013-06-19 20:11:55 +0800

    A shorter version.

--- 164-language-of-a-dfa.clj
+++ 164-language-of-a-dfa.clj
@@ -1,16 +1,14 @@
 (fn [dfa]
-  (let [accepts (:accepts dfa)
-        transitions (:transitions dfa)]
-    (letfn [(get-next-statuses [current-statuses]
-              (apply merge (flatten (map
-                                      (fn [entry]
-                                        (let [ns (get transitions (val entry))]
-                                          (if ns (map #(assoc {} (concat (key entry) [(key %)]) (val %)) ns))))
-                                      current-statuses))))
-            (step [current-statuses]
-              (let [next-statuses (get-next-statuses current-statuses)]
-                (if next-statuses
-                  (lazy-cat
-                    (map #(apply str (key %)) (filter #(accepts (val %)) next-statuses))
-                    (step next-statuses)))))]
-      (step {[] (:start dfa)}))))
+  (letfn [(get-next-statuses [current-statuses]
+            (apply merge (flatten (map
+                                    (fn [entry]
+                                      (let [ns (get (:transitions dfa) (val entry))]
+                                        (if ns (map #(assoc {} (concat (key entry) [(key %)]) (val %)) ns))))
+                                    current-statuses))))
+          (step [current-statuses]
+            (let [next-statuses (get-next-statuses current-statuses)]
+              (if next-statuses
+                (lazy-cat
+                  (map #(apply str (key %)) (filter #((:accepts dfa) (val %)) next-statuses))
+                  (step next-statuses)))))]
+    (step {[] (:start dfa)})))
